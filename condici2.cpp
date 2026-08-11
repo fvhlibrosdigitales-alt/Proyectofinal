6e7d413d772d782f3c84a9356d5568f30b56c4b1
fvhlibrosdigitales-alt <fvhlibrosdigitales@gmail.com>
2026-08-11 10:38:06 -0600

    Add files via upload

---
+++ condici2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int valor1 = 0, valor2=0;
+	
+		cout<<"ingrese un numero";
+		cin>>valor1;
+	
+	
+	if(valor1 >= 1 and valor1 <= 10 ){
+		//verdadero
+		cout<<"Es un numero natural";
+		
+	}else{
+		//falso
+		cout<<"NO es un numero natural NO";
+	}
+	
+	
+	
+	
+	
+	return 0;
+}
