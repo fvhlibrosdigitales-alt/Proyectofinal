481638289db7de6544f81444aa9d16e05bc15bc9
evalith2 <evalith2@gmail.com>
2026-08-14 12:32:32 -0600

    Update condici2.cpp

--- condici2.cpp
+++ condici2.cpp
@@ -1,8 +1,10 @@
 //Jeffersson
+
+//hola soy Alejandro alvarez:)
 #include <iostream>
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-//hola :)
+
 int main(int argc, char** argv) {
 	int valor1 = 0, valor2=0;
 	
