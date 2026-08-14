2a99cdf1389f3303b82b4a6fc83dd83e7146e4a6
evalith2 <evalith2@gmail.com>
2026-08-14 12:29:57 -0600

    Update condici2.cpp

--- condici2.cpp
+++ condici2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
+//hola :)
 int main(int argc, char** argv) {
 	int valor1 = 0, valor2=0;
 	
